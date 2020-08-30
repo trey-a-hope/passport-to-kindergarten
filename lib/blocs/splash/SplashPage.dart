1cdbe5ffabe7aef2928086fc67a50b5fffdad745
hopet1 <trey.a.hope@gmail.com>
2020-08-30 19:39:45 -0400

    Add Firebase GoogleService-Info.plist to project.

--- lib/blocs/splash/SplashPage.dart
+++ lib/blocs/splash/SplashPage.dart
@@ -1,7 +1,8 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:p/extensions/HexColorExtension.dart';
+import 'package:p/widgets/FullWidthButtonWidget.dart';
 import '../../constants.dart';
 import 'Bloc.dart';
 
@@ -44,70 +45,64 @@ class SplashPageState extends State<SplashPage> implements SplashDelegate {
                 child: SafeArea(
                   child: Stack(
                     children: [
-                      // Column(
-                      //   children: [
-                      //     Expanded(
-                      //       flex: 1,
-                      //       child: Container(),
-                      //     ),
-                      //     Expanded(
-                      //       flex: 1,
-                      //       child: Image.asset(
-                      //         ASSET_IMAGE_SPLASH_LOGO,
-                      //         width: screenWidth * 0.55,
-                      //       ),
-                      //     ),
-                      //     Expanded(
-                      //       flex: 1,
-                      //       child: Column(
-                      //         children: [
-                      //           Spacer(),
-                      //           FullWidthButtonWidget(
-                      //             buttonColor: Colors.white,
-                      //             text: 'Login',
-                      //             textColor: Colors.black,
-                      //             onPressed: () {
-                      //               Route route = MaterialPageRoute(
-                      //                 builder: (BuildContext context) =>
-                      //                     BlocProvider(
-                      //                   create: (BuildContext context) =>
-                      //                       LOGIN_BP.LoginBloc()
-                      //                         ..add(
-                      //                           LOGIN_BP.LoadPageEvent(),
-                      //                         ),
-                      //                   child: LOGIN_BP.LoginPage(),
-                      //                 ),
-                      //               );
-                      //               Navigator.push(context, route);
-                      //             },
-                      //           ),
-                      //           SizedBox(
-                      //             height: 20,
-                      //           ),
-                      //           FullWidthButtonWidget(
-                      //             buttonColor: HexColorExtension('e95316'),
-                      //             text: 'Sign Up',
-                      //             textColor: Colors.white,
-                      //             onPressed: () {
-                      //               Route route = MaterialPageRoute(
-                      //                 builder: (BuildContext context) =>
-                      //                     BlocProvider(
-                      //                   create: (BuildContext context) =>
-                      //                       SIGNUP_BP.SignupBloc()
-                      //                         ..add(
-                      //                           SIGNUP_BP.LoadPageEvent(),
-                      //                         ),
-                      //                   child: SIGNUP_BP.SignupPage(),
-                      //                 ),
-                      //               );
-                      //               Navigator.push(context, route);
-                      //             },
-                      //           ),
-                      //         ],
-                      //       ),
-                      //     ),
-                      //   ],
-                      // ),
+                      Column(
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Container(),
+                          ),
+                          Expanded(flex: 1, child: Container()),
+                          Expanded(
+                            flex: 1,
+                            child: Column(
+                              children: [
+                                Spacer(),
+                                FullWidthButtonWidget(
+                                  buttonColor: Colors.white,
+                                  text: 'Login',
+                                  textColor: Colors.black,
+                                  onPressed: () {
+                                    // Route route = MaterialPageRoute(
+                                    //   builder: (BuildContext context) =>
+                                    //       BlocProvider(
+                                    //     create: (BuildContext context) =>
+                                    //         LOGIN_BP.LoginBloc()
+                                    //           ..add(
+                                    //             LOGIN_BP.LoadPageEvent(),
+                                    //           ),
+                                    //     child: LOGIN_BP.LoginPage(),
+                                    //   ),
+                                    // );
+                                    // Navigator.push(context, route);
+                                  },
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                FullWidthButtonWidget(
+                                  buttonColor: HexColorExtension('e95316'),
+                                  text: 'Sign Up',
+                                  textColor: Colors.white,
+                                  onPressed: () {
+                                    // Route route = MaterialPageRoute(
+                                    //   builder: (BuildContext context) =>
+                                    //       BlocProvider(
+                                    //     create: (BuildContext context) =>
+                                    //         SIGNUP_BP.SignupBloc()
+                                    //           ..add(
+                                    //             SIGNUP_BP.LoadPageEvent(),
+                                    //           ),
+                                    //     child: SIGNUP_BP.SignupPage(),
+                                    //   ),
+                                    // );
+                                    // Navigator.push(context, route);
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ],
                   ),
                 ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:p/models/UserModel.dart';
+import 'package:p/services/UserService.dart';
+import 'package:p/widgets/FullWidthButtonWidget.dart';
 import 'package:package_info/package_info.dart';
 import 'ServiceLocator.dart';
 import 'blocs/splash/Bloc.dart';
 import 'constants.dart';
+import 'services/AuthService.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,48 +31,38 @@ class MyApp extends StatelessWidget {
     );
 
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.orange,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: Container(
-          child: Center(
-            child: Text('Welcome To P2K'),
-          ),
-        )
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: StreamBuilder(
+        stream: locator<AuthService>().onAuthStateChanged(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          final FirebaseUser firebaseUser = snapshot.data;
 
-        // BlocProvider(
-        //   create: (BuildContext context) => SplashBloc()..add(LoadPageEvent()),
-        //   child: SplashPage(),
-        // )
-
-        // StreamBuilder(
-        //   stream: locator<AuthService>().onAuthStateChanged(),
-        //   builder: (BuildContext context, AsyncSnapshot snapshot) {
-        //     final FirebaseUser firebaseUser = snapshot.data;
-        //     return firebaseUser == null
-        //         ? BlocProvider(
-        //             create: (BuildContext context) =>
-        //                 SplashBloc()..add(LoadPageEvent()),
-        //             child: SplashPage(),
-        //           )
-        //         : Container(
-        //             color: Colors.orange,
-        //             child: Center(
-        //               child: FullWidthButtonWidget(
-        //                 buttonColor: Colors.white,
-        //                 text: 'Sign Out',
-        //                 textColor: Colors.orange,
-        //                 onPressed: () {
-        //                   locator<AuthService>().signOut();
-        //                 },
-        //               ),
-        //             ),
-        //           );
-        //   },
-        // ),
-        );
+          return firebaseUser == null
+              ? BlocProvider(
+                  create: (BuildContext context) =>
+                      SplashBloc()..add(LoadPageEvent()),
+                  child: SplashPage(),
+                )
+              : Container(
+                  color: Colors.orange,
+                  child: Center(
+                    child: FullWidthButtonWidget(
+                      buttonColor: Colors.white,
+                      text: 'Sign Out',
+                      textColor: Colors.orange,
+                      onPressed: () {
+                        locator<AuthService>().signOut();
+                      },
+                    ),
+                  ),
+                );
+        },
+      ),
+    );
   }
 }
