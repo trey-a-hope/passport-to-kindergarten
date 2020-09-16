438100fec85af4704e0beb8709b3d43f9e8d6f27
Trey Hope <trey.a.hope@gmail.com>
2020-09-15 22:07:06 -0400

    Added confetti package.

--- lib/blocs/home/HomePage.dart
+++ lib/blocs/home/HomePage.dart
@@ -1,3 +1,4 @@
+import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -14,12 +15,22 @@ class HomePage extends StatefulWidget {
 class HomePageState extends State<HomePage> implements HomeDelegate {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   HomeBloc _homeBloc;
+  ConfettiController _confettiController;
 
   @override
   void initState() {
     _homeBloc = BlocProvider.of<HomeBloc>(context);
     _homeBloc.setDelegate(delegate: this);
+    _confettiController = ConfettiController(
+      duration: const Duration(seconds: 1),
+    );
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _confettiController.dispose();
+    super.dispose();
   }
 
   @override
@@ -45,8 +56,49 @@ class HomePageState extends State<HomePage> implements HomeDelegate {
             ),
             body: AnnotatedRegion<SystemUiOverlayStyle>(
               value: SystemUiOverlayStyle.light,
-              child: Center(
-                child: Text('Home Page'),
+              child: Stack(
+                children: [
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: ConfettiWidget(
+                      numberOfParticles: 10,
+                      confettiController: _confettiController,
+                      blastDirectionality: BlastDirectionality
+                          .explosive, // don't specify a direction, blast randomly
+                      shouldLoop:
+                          true, // start again as soon as the animation is finished
+                      colors: const [
+                        Colors.green,
+                        Colors.blue,
+                        Colors.pink,
+                        Colors.orange,
+                        Colors.purple
+                      ], // manually specify the colors to be used
+                    ),
+                  ),
+                  Center(
+                      child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      RaisedButton(
+                        child: Text('Start Confetti'),
+                        color: Colors.green,
+                        textColor: Colors.white,
+                        onPressed: () {
+                          _confettiController.play();
+                        },
+                      ),
+                      RaisedButton(
+                        child: Text('Stop Confetti'),
+                        color: Colors.red,
+                        textColor: Colors.white,
+                        onPressed: () {
+                          _confettiController.stop();
+                        },
+                      )
+                    ],
+                  )),
+                ],
               ),
             ),
           );
