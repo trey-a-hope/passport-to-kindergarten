38b527d847c8078ab5d2eed99c508d7b85d6d109
Trey Hope <trey.a.hope@gmail.com>
2020-10-08 05:33:41 -0400

    Updated UI for Book of the Month

--- lib/blocs/bookOfTheMonth/BookOfTheMonthPage.dart
+++ lib/blocs/bookOfTheMonth/BookOfTheMonthPage.dart
@@ -53,68 +53,96 @@ class BookOfTheMonthPageState extends State<BookOfTheMonthPage>
           return Scaffold(
             key: _scaffoldKey,
             appBar: AppBar(
-              title: Text('Book of The Month'),
-            ),
-            drawer: DrawerWidget(
-              currentUser: state.user,
-              page: APP_PAGES.BOOK_OF_THE_MONTH,
+              iconTheme: IconThemeData(color: Colors.black),
+              backgroundColor: COLOR_CREAM,
             ),
             body: AnnotatedRegion<SystemUiOverlayStyle>(
               value: SystemUiOverlayStyle.light,
-              child: GridView.builder(
-                itemCount: BOOKS_OF_THE_MONTH.length,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2),
-                itemBuilder: (BuildContext context, int index) {
-                  final BookOfTheMonthModel bookOfTheMonth =
-                      BOOKS_OF_THE_MONTH[index];
-
-                  return Padding(
-                    padding: EdgeInsets.all(10),
-                    child: InkWell(
-                      child: Container(
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            colorFilter: ColorFilter.mode(
-                                Colors.black.withOpacity(0.3),
-                                BlendMode.darken),
-                            fit: BoxFit.cover,
-                            image: NetworkImage('${bookOfTheMonth.imgUrl}'),
-                          ),
-                          color: Colors.green,
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(20),
-                          ),
-                        ),
+              child: Container(
+                width: screenWidth,
+                height: screenHeight,
+                color: COLOR_CREAM,
+                child: SafeArea(
+                  child: Column(
+                    children: [
+                      Container(
+                        width: screenWidth,
+                        height: 80,
+                        color: COLOR_ORANGE,
                         child: Center(
                           child: Text(
-                            '${bookOfTheMonth.title}',
+                            'Book of The Month',
                             style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold),
-                            textAlign: TextAlign.center,
+                              color: Colors.white,
+                              fontSize: 21,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
-                      onTap: () {
-                        Route route = MaterialPageRoute(
-                          builder: (BuildContext context) => BlocProvider(
-                            create: (BuildContext context) =>
-                                BOOK_OF_THE_MONTH_DETAILS_BP
-                                    .BookOfTheMonthDetailsBloc()
-                                  ..add(
-                                    BOOK_OF_THE_MONTH_DETAILS_BP
-                                        .LoadPageEvent(),
+                      Expanded(
+                        child: GridView.builder(
+                          itemCount: BOOKS_OF_THE_MONTH.length,
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 2),
+                          itemBuilder: (BuildContext context, int index) {
+                            final BookOfTheMonthModel bookOfTheMonth =
+                                BOOKS_OF_THE_MONTH[index];
+
+                            return Padding(
+                              padding: EdgeInsets.all(10),
+                              child: InkWell(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      colorFilter: ColorFilter.mode(
+                                          Colors.black.withOpacity(0.3),
+                                          BlendMode.darken),
+                                      fit: BoxFit.cover,
+                                      image: NetworkImage(
+                                          'https://picsum.photos/200'),
+                                    ),
+                                    color: Colors.green,
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(20),
+                                    ),
                                   ),
-                            child: BOOK_OF_THE_MONTH_DETAILS_BP
-                                .BookOfTheMonthDetailsPage(),
-                          ),
-                        );
-                        Navigator.push(context, route);
-                      },
-                    ),
-                  );
-                },
+                                  child: Center(
+                                    child: Text(
+                                      '${bookOfTheMonth.title}',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ),
+                                ),
+                                onTap: () {
+                                  Route route = MaterialPageRoute(
+                                    builder: (BuildContext context) =>
+                                        BlocProvider(
+                                      create: (BuildContext context) =>
+                                          BOOK_OF_THE_MONTH_DETAILS_BP
+                                              .BookOfTheMonthDetailsBloc()
+                                            ..add(
+                                              BOOK_OF_THE_MONTH_DETAILS_BP
+                                                  .LoadPageEvent(),
+                                            ),
+                                      child: BOOK_OF_THE_MONTH_DETAILS_BP
+                                          .BookOfTheMonthDetailsPage(),
+                                    ),
+                                  );
+                                  Navigator.push(context, route);
+                                },
+                              ),
+                            );
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               ),
             ),
           );
