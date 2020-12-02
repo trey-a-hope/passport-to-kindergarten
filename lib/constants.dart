793f521931b68ac608f85f0b6a4c4bf4b1a25107
Trey Hope <trey.a.hope@gmail.com>
2020-12-01 21:13:02 -0500

    Updated contact email.

--- lib/constants.dart
+++ lib/constants.dart
@@ -82,7 +82,7 @@ const String DUMMY_PROFILE_PHOTO_URL =
     'https://firebasestorage.googleapis.com/v0/b/hidden-gems-e481d.appspot.com/o/Images%2FUsers%2FDummy%2FProfile.jpg?alt=media&token=99cd4cbd-7df9-4005-adef-b27b3996a6cc';
 
 //Phone & Email
-const String COMPANY_EMAIL = 'test@gmail.com';
+const String COMPANY_EMAIL = 'passport@preschoolpromise.org';
 
 //COLORS
 final HexColorExtension COLOR_CREAM = HexColorExtension('#fff8ec');
